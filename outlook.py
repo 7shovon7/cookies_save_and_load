ad2dcfbb8a8408b0cc8048758e3797b3a4a7d2ef
7shovon7 <mahmudur.rahman99@gmail.com>
2020-06-20 03:58:59 +0600

    added mailing draft creation automation on outlook

---
+++ outlook.py
@@ -0,0 +1,38 @@
+import time
+import pickle
+from selenium import webdriver
+
+
+# INITIAL COOKIE LOADING
+def ln_login(driver, config_file, cookies_file):
+    driver.get("https://www.linkedin.com/login")
+    file = open(config_file)
+    lines = file.readlines()
+    username = lines[0]
+    password = lines[1]
+    element_id = driver.find_element_by_id("username")
+    element_id.send_keys(username)
+    element_id = driver.find_element_by_id("password")
+    element_id.send_keys(password)
+    element_id.submit()
+    pickle.dump(driver.get_cookies(), open(cookies_file, "wb"))
+
+
+def load_cookies(driver, location, url=None):
+    cookies = pickle.load(open(location, "rb"))
+    driver.delete_all_cookies()
+    url = "https://www.google.com" if url is None else url
+    driver.get(url)
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+
+
+chrome = webdriver.Chrome()
+# TO LOGIN FOR THE FIRST TIME
+# ln_login(chrome, "config.txt", "cookies.txt")
+
+# FOR FURHTER USE
+ln_url = "https://www.linkedin.com"
+g_url = "https://google.com"
+load_cookies(chrome, "cookies.txt")
+chrome.get(ln_url)
